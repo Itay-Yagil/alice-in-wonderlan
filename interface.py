0cd20f1b5cba9e4a10d533fb75fdcb02e660af7e
ItayYagil <132233086+ItayYagil@users.noreply.github.com>
2026-03-12 11:40:19 +0200

    fixes the router

--- interface.py
+++ interface.py
@@ -1,5 +1,17 @@
+import ipaddress
+
 class Interface:
     def __init__(self, name, ip, mac):
-        self.name = name
-        self.ip = ip
-        self.mac = mac
+        self._name = name
+        self._ip = ip
+        self._mac = mac
+
+    def get_name(self):
+        return self._name
+    
+    def get_mac(self):
+        return self._mac
+    
+    def get_ip(self):
+        return self._ip
+    
--- mad_router.py
+++ mad_router.py
@@ -1,34 +1,35 @@
 import scapy.all as s
 from interface import Interface
-from routing_table import RoutingTable, Entry
 
 class Router:
-    def __init__(self, ifaces, routing_table):
-        self._ifaces = ifaces
-        self._routing_table = routing_table
+    def __init__(self, in_iface, out_iface):
+        self._in_iface = in_iface
+        self._out_iface = out_iface
     
     def run(self):
         while True:
-            packet = s.sniff(iface=[iface.name for iface in self._ifaces], count=1)[0]
+            packet = s.sniff(iface=[self._in_iface.name, self._out_iface.name], count=1)[0]
             packet.show()
             if s.IP not in packet:
                 continue
-            out_iface = self._routing_table.find_match(packet[s.IP].dst)
             print(f"Recieved from {packet.sniffed_on}")
-            if out_iface != None:
-                print(f"Sending to {out_iface.name}")
-                packet.src = out_iface.mac
-                packet[s.IP].ttl -= 1
-                packet.show()
-                s.sendp(packet, iface=out_iface.name)
+
+            send_to_iface = None
+            if packet.sniffed_on == self._in_iface.name:
+                send_to_iface = self._out_iface
+            else:
+                send_to_iface = self._in_iface
+            print(f"Sending to {send_to_iface.name}")
+            packet.src = send_to_iface.mac
+            packet[s.IP].ttl -= 1
+            packet.show()
+            s.sendp(packet, iface=send_to_iface.name)
             
 
 def main():
-    iface1 = Interface("enp0s8", "192.168.56.101", "08:00:27:ad:b5:99")
-    iface2 = Interface("enp0s9", "192.168.56.102", "08:00:27:86:a9:6f")
-    ifaces = [iface1, iface2]
-    routing_table = RoutingTable([Entry(iface1, "192.168.56.101/32"), Entry(iface2, "0.0.0.0/0")])
-    router = Router(ifaces, routing_table)
+    iface1 = Interface("enp0s8", "192.168.64.3", "08:00:27:ad:b5:99")
+    iface2 = Interface("enp0s9", "192.168.36.3", "08:00:27:86:a9:6f")
+    router = Router(iface1, iface2)
     router.run()
 
     
